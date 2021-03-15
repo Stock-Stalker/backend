7c7f9644b0659ef9bf6fa52db345b80e3b57f181
Starlight Romero <starlightromero@protonmail.com>
2021-03-15 16:44:57 -0700

    [update] backend added getRepos route with cache

--- backend/routes/main.js
+++ backend/routes/main.js
@@ -2,7 +2,11 @@ const express = require('express')
 
 const router = express.Router()
 
+const cache = require('../middleware/cache')
+
 const mainController = require('../controllers/main')
+
+router.get('/repos/:username', cache.cache, mainController.getRepos)
 
 router.get('/', mainController.getHome)
 
