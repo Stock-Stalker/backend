40f4c041058b7e89f603d236cf7c7dafe375067b
Starlight Romero <starlightromero@protonmail.com>
2021-03-16 12:55:47 -0700

    [update] stock contoller to get company name from cache

--- backend/controllers/stock.js
+++ backend/controllers/stock.js
@@ -1,20 +1,15 @@
 const { getCompanyName, getHistoricalData } = require('../utils/stock')
-const { isCacheDataValid, getHistoricalDataFromCache } = require('../utils/cache')
+const { getCompanyNameFromCache } = require('../utils/cache')
 
 exports.getStockData = async (req, res) => {
   const symbol = req.params.symbol.toUpperCase()
-  let stockData
-  if (isCacheDataValid(symbol)) {
-    stockData = getHistoricalDataFromCache(symbol)
-  } else {
-    const companyName = getCompanyName(symbol) // will be changed to get from mongo
-    const historicalData = await getHistoricalData(symbol)
-    stockData = {
-      symbol,
-      companyName,
-      historicalData,
-      'currentPrice': historicalData[0].close
-    }
+  const companyName = await getCompanyNameFromCache(symbol) || await getCompanyName(symbol) // will be changed to get from mongo
+  const historicalData = await getHistoricalData(symbol)
+  const stockData = {
+    symbol,
+    companyName,
+    historicalData,
+    'currentPrice': historicalData[0].close
   }
   res.send({ stockData })
 }
