e5e5c9dffe83d89b61ea75427d755755d16e2501
Yin <yinnnn17@gmail.com>
2021-03-17 10:53:16 -0700

    pull change

---
+++ backend/routes/auth.js
@@ -0,0 +1,39 @@
+const express = require('express')
+
+const { body } = require('express-validator')
+
+const authController = require('../controllers/auth')
+
+const isAuth = require('../middleware/isAuth')
+
+const router = express.Router()
+
+router.post('/signup', [
+  body('username', 'Please make sure your username is between 4 and 25 characters and contains no special characters.')
+    .isLength({ min: 4, max: 25 })
+    .escape()
+    .not()
+    .isEmpty(),
+  body('password', 'Please make sure your password is between 8 and 25 characters and contains no special characters.')
+    .isLength({ min: 8, max: 25 })
+    .escape()
+    .not()
+    .isEmpty()
+], authController.signUpUser)
+
+router.post('/signin', [
+  body('username', 'Please make sure your username is between 4 and 25 characters and contains no special characters.')
+    .isLength({ min: 4, max: 25 })
+    .escape()
+    .not()
+    .isEmpty(),
+  body('password', 'Please make sure your password is between 8 and 25 characters and contains no special characters.')
+    .isLength({ min: 8, max: 25 })
+    .escape()
+    .not()
+    .isEmpty()
+], authController.signInUser)
+
+router.get('/signout', isAuth, authController.signOutUser)
+
+module.exports = router
