337fd9e95f2169d561bce4f4fbf6fa627b34ec69
Yin <yinnnn17@gmail.com>
2021-03-18 10:41:15 -0700

    fixed linter error

--- backend/tests/watchlist.js
+++ backend/tests/watchlist.js
@@ -1,26 +1,26 @@
-const app = require('../app');
-const chaiHttp = require('chai-http');
-const chai = require('chai');
-const mongoose = require('mongoose');
+const app = require('../app')
+const chaiHttp = require('chai-http')
+const chai = require('chai')
+const mongoose = require('mongoose')
 
-const should = chai.should();
+const should = chai.should()
 const { expect } = chai;
 
 chai.use(chaiHttp);
-const agent = chai.request.agent(app);
+const agent = chai.request.agent(app)
 
-const User = require('../models/user');
+const User = require('../models/user')
 
-let token;
+let token
 
 /**
  * root level hooks
  */
 after(function (done) {
-  mongoose.models = {};
-  mongoose.modelSchemas = {};
-  mongoose.connection.close();
-  done();
+  mongoose.models = {}
+  mongoose.modelSchemas = {}
+  mongoose.connection.close()
+  done()
 });
 
 const SAMPLE_OBJECT_ID = 'aaaaaaaaaaaa'; // 12 byte string
@@ -33,26 +33,26 @@ describe('Authentication API endpoints', function () {
       password: 'mypassword',
       _id: SAMPLE_OBJECT_ID,
     });
-      chai.request(app)
-        .post('/api/user/signup')
-        .set('content-type', 'application/json')
-        .send(sampleUser)
-        .end((err, res) => {
-          if (err) { done(err); }
-          sampleUser.save()
-          done();
-        });
-  });
+    chai.request(app)
+      .post('/api/user/signup')
+      .set('content-type', 'application/json')
+      .send(sampleUser)
+      .end((err, res) => {
+        if (err) { done(err) }
+        sampleUser.save()
+        done()
+      })
+  })
 
   // Delete sample user.
   afterEach(function (done) {
     User.deleteMany({ username: 'myuser' })
       .then(function () {
-        done();
-      });
-  });
+        done()
+      })
+  })
 
-  it('should get all stock data in user\' watchlist', function (done) {
+  it('should get all stock data in users watchlist', function (done) {
     chai.request(app)
       .post('/api/user/watchlist')
       .end(function (err, res) {
@@ -60,6 +60,6 @@ describe('Authentication API endpoints', function () {
         console.log(`!!!!!! ${res.body}`)
         expect(res.body).to.be.an('Array');
         // expect(res.body.user).to.have.property('username', 'anotheruser');
-        });
-      });
-});
+      })
+  })
+})
--- backend/utils/watchlist.js
+++ backend/utils/watchlist.js
@@ -1,23 +1,23 @@
-const User = require("../models/user")
+const User = require('../models/user')
 const Symbols = require('../models/symbols')
 
 const getStock = (symbol) => new Promise ((resolve, reject) => {
-  Symbols.findOne({symbol:symbol})
-  .then((stock)=>resolve(stock))
-  .catch((error)=>reject(error))
+  Symbols.findOne({symbol: symbol})
+    .then((stock) => resolve(stock))
+    .catch((error) => reject(error))
 })
 const getUser = async (userId) => {
-  try{
-    const user = await User.findOne({_id:userId})
+  try {
+    const user = await User.findOne({ _id: userId })
     return user
-  } catch (err){
+  } catch (err) {
     return err
-  }  
+  }
 }
 
-const checkDuplicate = (stockId,watchlist) => {
-  for(let i=0;i<watchlist.length;i++){
-    if(String(stockId) === String(watchlist[i]._id)){
+const checkDuplicate = (stockId, watchlist) => {
+  for (let i = 0; i < watchlist.length; i++) {
+    if (String(stockId) === String(watchlist[i]._id)) {
       return true
     }
   } return false
