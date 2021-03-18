e5b679da2c86781b30e70533fc0f9d3008d84793
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-03-18 14:19:03 -0700

    [update] refactored all watchlist controllers to use async/await syntax.

--- backend/controllers/watchlist.js
+++ backend/controllers/watchlist.js
@@ -4,16 +4,16 @@ const User = require("../models/user")
 const Symbols = require('../models/symbols')
 
 exports.getwatchlist = async (req,res) =>{
-  try{
+  try {
     const user = await getUser(req.userId)
     // TODO: populate this so we don't just get the id reference back
     res.send(user.watchlist)
-  }catch (err) {
+  } catch (err) {
     res.status(403).send({ message: err.message })
   }
 }
 
-exports.addToWatchlist = async (req,res) =>{
+exports.addToWatchlist = async (req,res) => {
   try{
     const user = await getUser(req.userId)
     const stock = await getStock(req.body.symbol)
@@ -29,10 +29,11 @@ exports.addToWatchlist = async (req,res) =>{
 
 exports.removeFromWatchlist = async (req,res) => {
   try {
-    const symbolToRemove = await Symbols.findOne({symbol:req.params.symbol});
+    const symbolToRemove = await Symbols.findOne({ symbol: req.body.symbol });
     const symbolID = symbolToRemove._id;
+    console.log(`symbolId: ${symbolID}`)
     const updatedUser = await User.findByIdAndUpdate(req.userId,
-        { $pull: {watchlist:symbolID}},{new: true}).populate('watchlist');
+        { $pull: { watchlist: {_id: symbolID.toString()} }},{new: true}).populate('watchlist');
     return res.status(200).send(updatedUser.watchlist);
   } catch (err) {
     return res.status(403).send({ message: err.message });
