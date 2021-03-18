7cd5cc4d596216b09617fcc46d1597e989da69fe
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-03-18 14:22:07 -0700

    [update] fixed merge conflicts

--- backend/controllers/watchlist.js
+++ backend/controllers/watchlist.js
@@ -2,24 +2,6 @@ const { getStock, getUser, checkDuplicate } = require('../utils/watchlist')
 const User = require('../models/user')
 const Symbols = require('../models/symbols')
 
-<<<<<<< HEAD
-exports.getwatchlist = async (req,res) =>{
-  try {
-    const user = await getUser(req.userId)
-    // TODO: populate this so we don't just get the id reference back
-    res.send(user.watchlist)
-  } catch (err) {
-    res.status(403).send({ message: err.message })
-  }
-}
-
-exports.addToWatchlist = async (req,res) => {
-  try{
-    const user = await getUser(req.userId)
-    const stock = await getStock(req.body.symbol)
-    if (!checkDuplicate(stock._id,user.watchlist)) {
-      user.watchlist.unshift(stock);
-=======
 exports.getwatchlist = async (req, res) => {
     try {
         const user = await getUser(req.userId)
@@ -41,11 +23,9 @@ exports.addToWatchlist = async (req, res) => {
         res.send(user.watchlist)
     } catch (err) {
         res.status(403).send({ message: err.message })
->>>>>>> 7324d80e14b2483d507d78b7e9e4b74fa8be5ab4
     }
 }
 
-<<<<<<< HEAD
 exports.removeFromWatchlist = async (req,res) => {
   try {
     const symbolToRemove = await Symbols.findOne({ symbol: req.body.symbol });
@@ -57,21 +37,4 @@ exports.removeFromWatchlist = async (req,res) => {
   } catch (err) {
     return res.status(403).send({ message: err.message });
   }
-=======
-exports.removeFromWatchlist = async (req, res) => {
-    try {
-        const symbolToRemove = await Symbols.findOne({
-            symbol: req.params.symbol
-        })
-        const symbolID = symbolToRemove._id
-        const updatedUser = await User.findByIdAndUpdate(
-            req.userId,
-            { $pull: { watchlist: symbolID } },
-            { new: true }
-        ).populate('watchlist')
-        return res.status(200).send(updatedUser.watchlist)
-    } catch (err) {
-        return res.status(403).send({ message: err.message })
-    }
->>>>>>> 7324d80e14b2483d507d78b7e9e4b74fa8be5ab4
 }
--- backend/routes/watchlist.js
+++ backend/routes/watchlist.js
@@ -10,10 +10,6 @@ router.get('/', isAuth, watchlistController.getwatchlist)
 
 router.patch('/', isAuth, watchlistController.addToWatchlist)
 
-<<<<<<< HEAD
 router.patch('/remove',isAuth, watchlistController.removeFromWatchlist)
-=======
-router.patch('/remove/:symbol', isAuth, watchlistController.removeFromWatchlist)
->>>>>>> 7324d80e14b2483d507d78b7e9e4b74fa8be5ab4
 
 module.exports = router
