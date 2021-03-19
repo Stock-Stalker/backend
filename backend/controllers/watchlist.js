17af8ed9b627970252c0eb7d774540a5df2ec15f
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-03-19 10:25:36 -0700

    [update] tested watchlist duplicate avoidance (addToSet) and remove watchlist item. Fixed watchlist remove function.

--- backend/controllers/watchlist.js
+++ backend/controllers/watchlist.js
@@ -2,7 +2,6 @@ const { getStock, getUser, AddSymbol } = require('../utils/watchlist')
 const User = require('../models/user')
 const Symbols = require('../models/symbols')
 
-
 exports.getwatchlist = async (req, res) => {
     try {
         const user = await getUser(req.userId)
@@ -32,13 +31,10 @@ exports.removeFromWatchlist = async (req, res) => {
             symbol: req.body.symbol
         })
         const symbolID = symbolToRemove._id
-        console.log(`symbolId: ${symbolID}`)
-        const updatedUser = await User.findByIdAndUpdate(
-            req.userId,
-            { $pull: { watchlist: { _id: symbolID.toString() } } },
-            { new: true }
-        ).populate('watchlist')
-        return res.status(200).send(updatedUser.watchlist)
+        const user = await User.findOne({ _id: req.userId })
+        const updatedWatchlist = await user.watchlist.pull({ _id: symbolID })
+        user.save()
+        return res.status(200).send(user.watchlist)
     } catch (err) {
         return res.status(403).send({ message: err.message })
     }
