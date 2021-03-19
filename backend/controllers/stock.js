0eafb4fdf3ba2c6a0b6a7df2f911877a8fbcb2c9
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-03-18 21:54:40 -0700

    [update] completed call to /predictor from Node API. Troubleshooting circular structure issue.

--- backend/controllers/stock.js
+++ backend/controllers/stock.js
@@ -4,6 +4,8 @@ const {
     getAllStockData
 } = require('../utils/stock')
 const { getCompanyNameFromCache } = require('../utils/cache')
+const axios = require('axios')
+const { parse, stringify } = require('flatted')
 
 exports.getAllStocks = async (req, res) => {
     try {
@@ -30,5 +32,17 @@ exports.getStockData = async (req, res) => {
         res.send({ stockData })
     } catch (err) {
         res.status(404).send({ message: err.message })
+    }
+}
+
+exports.getStockPrediction = async (req, res) => {
+    try {
+        const symbol = req.params.symbol
+        const p = await axios.get(`http://stockstalker.tk/predictor/${symbol}`)
+        console.log(`prediction: ${p.data.toString()}`)
+        return res.status(200).json({ prediction: p.data.predictions.toString() })
+        // return res.status(200).send(p)
+    } catch (err) {
+        res.status(500).send({ message: err.message })
     }
 }
--- backend/routes/stock.js
+++ backend/routes/stock.js
@@ -4,10 +4,10 @@ const router = express.Router()
 
 const stockController = require('../controllers/stock')
 
+router.get('/prediction/:symbol', stockController.getStockPrediction)
+
 router.get('/', stockController.getAllStocks)
 
 router.get('/:symbol', stockController.getStockData)
-
-router.get('/prediction/:symbol', stockController.getStockData)
 
 module.exports = router
