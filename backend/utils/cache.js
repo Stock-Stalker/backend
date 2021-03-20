8aecd7022d31735a6f0c5f0a9641ab4c501df59d
Starlight Romero <starlightromero@protonmail.com>
2021-03-20 10:35:19 -0700

    [update] add getPredictionFromCache util func

--- backend/utils/cache.js
+++ backend/utils/cache.js
@@ -8,12 +8,22 @@ const getCompanyNameFromCache = (symbol) => {
     client.get(symbol, (err, companyName) => {
         if (err) throw err
         if (companyName === null) return null
-        console.log('\nUsing Cache\n')
+        console.log('\nUsing cache for company name\n')
         return companyName
+    })
+}
+
+const getPredictionFromCache = (symbol) => {
+    client.get(`${symbol}_predict`, (err, prediction) => {
+        if (err) throw err
+        if (prediction === null) return null
+        console.log('\nUsing cache for prediction\n')
+        return prediction
     })
 }
 
 module.exports = {
     client,
-    getCompanyNameFromCache
+    getCompanyNameFromCache,
+    getPredictionFromCache
 }
