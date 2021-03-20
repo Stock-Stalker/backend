dc01c46b0cf6982f1e948df9624adcec89bce40a
Starlight Romero <starlightromero@protonmail.com>
2021-03-19 22:01:22 -0700

    [delete] main routes

--- backend/routes/main.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const router = express.Router()
-
-const mainController = require('../controllers/main')
-
-router.get('/', mainController.getHome)
-
-module.exports = router
