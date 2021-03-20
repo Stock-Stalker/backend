88fe5791676c3b012a0fddff4572f97ef8f0b7e0
Starlight Romero <starlightromero@protonmail.com>
2021-03-19 22:03:10 -0700

    [update] watchlist contoller refactor to optimize

--- backend/controllers/watchlist.js
+++ backend/controllers/watchlist.js
@@ -1,9 +1,9 @@
-const { getStock, getUser } = require('../utils/watchlist')
 const User = require('../models/user')
+const Symbols = require('../models/symbols')
 
 exports.getWatchlist = async (req, res) => {
     try {
-        const user = await getUser(req.userId)
+        const user = await User.findOne({ _id: req.userId })
         return res.status(200).send(user.watchlist)
     } catch (err) {
         return res.status(403).send({ message: err.message })
@@ -12,16 +12,17 @@ exports.getWatchlist = async (req, res) => {
 
 exports.updateWatchlist = async (req, res) => {
     try {
-        const targetSymbol = await getStock(req.body.symbol)
-        const user = await getUser(req.userId)
-        const isExist = await User.findOne({
+        const symbol = req.body.symbol.toUpperCase()
+        let user = await User.findOne({
             _id: req.userId,
-            watchlist: targetSymbol
+            watchlist: { $elemMatch: { symbol: symbol } }
         })
-        if (isExist) {
-            await user.watchlist.pull({ _id: targetSymbol._id })
+        if (user) {
+            await user.watchlist.pull({ symbol: symbol })
         } else {
-            await user.watchlist.addToSet(targetSymbol)
+            const stock = await Symbols.findOne({ symbol: symbol })
+            user = await User.findOne({ _id: req.userId })
+            await user.watchlist.addToSet(stock)
         }
         await user.save()
         return res.status(200).send(user.watchlist)
