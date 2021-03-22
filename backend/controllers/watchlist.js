320fc7232c89c4aae716915217727d270e7fa6fd
Starlight Romero <starlightromero@protonmail.com>
2021-03-22 14:59:44 -0700

    [update] watchlist controllers removed unused logs

--- backend/controllers/watchlist.js
+++ backend/controllers/watchlist.js
@@ -8,22 +8,16 @@ exports.getWatchlist = async (req, res) => {
         const user = await User.findOne({ _id: req.userId })
         const watchlist = JSON.parse(JSON.stringify(user.watchlist))
         const stocksNeededFromAPI = []
-        console.log('WATCHLIST FIRST', watchlist)
         for (const stock of watchlist) {
             const prediction = await getPredictionFromCache(stock.symbol)
             console.log('FOR LOOP', prediction)
-            if (prediction) {
-                console.log('PREDICTION DATA DATA', prediction.data.data)
-                stock.prediction = prediction.data
-            } else {
-                console.log('PUSH!')
-                stocksNeededFromAPI.push(stock.symbol)
-            }
+            prediction
+                ? (stock.prediction = prediction.data)
+                : stocksNeededFromAPI.push(stock.symbol)
         }
         if (stocksNeededFromAPI.length > 0) {
-            console.log('NEED FROM API')
-            const predictions = await getPredictionsFromAPI(stocksNeededFromAPI)
-            console.log('PREDICTIONS', predictions)
+            const res = await getPredictionsFromAPI(stocksNeededFromAPI)
+            console.log('PREDICTIONS', res.data)
             // prediction
             //     ? (stock.prediction = prediction.data)
             //     : stocksNeededFromAPI.push(stock.symbol)
