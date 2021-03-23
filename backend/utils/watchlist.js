c411ef891fc8bba95154e1a7e6244c6469cb19b4
Starlight Romero <starlightromero@protonmail.com>
2021-03-22 18:43:21 -0700

    [create] updateWatchlistDetails util func

---
+++ backend/utils/watchlist.js
@@ -0,0 +1,31 @@
+const { getCurrentPrices, getPredictionsFromAPI } = require('./stock')
+const { getPredictionFromCache } = require('./cache')
+
+const updateWatchlistDetails = async (user, watchlist) => {
+    const stocksNeededFromAPI = []
+    const watchlistSymbolList = []
+    for (const stock of watchlist) {
+        watchlistSymbolList.push(stock.symbol)
+        const prediction = await getPredictionFromCache(stock.symbol)
+        prediction
+            ? (stock.prediction = prediction)
+            : stocksNeededFromAPI.push(stock.symbol)
+    }
+    const currentPrices = await getCurrentPrices(watchlistSymbolList)
+    let predictions
+    if (stocksNeededFromAPI.length > 0) {
+        predictions = await getPredictionsFromAPI(stocksNeededFromAPI)
+    }
+    for (const stock of watchlist) {
+        stock.currentPrice = currentPrices[stock.symbol].price
+        if (predictions[stock.symbol]) {
+            stock.prediction = predictions[stock.symbol]
+        }
+    }
+    await user.save()
+    return user
+}
+
+module.exports = {
+    updateWatchlistDetails
+}
