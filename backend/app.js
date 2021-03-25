d3d4b3c0aa57f9b09b4030ed901789b6b43b09d0
Starlight Romero <starlightromero@protonmail.com>
2021-03-25 15:48:46 -0700

    [update] added cors

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const cors = require('cors')
 const express = require('express')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
@@ -7,21 +8,32 @@ const authRoutes = require('./routes/auth')
 const watchlistRoutes = require('./routes/watchlist')
 const app = express()
 
+const corsOptions = {
+    origin: [
+        'http://localhost:8080',
+        'http://localhost:8100',
+        'http://stockstalker.tk',
+        'https://stockstalker.tk'
+    ],
+    credentials: true
+}
+
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(cookieParser())
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET, PUT, POST, PATCH, DELETE'
-    )
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    )
-    next()
-})
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*')
+//     res.setHeader(
+//         'Access-Control-Allow-Methods',
+//         'GET, PUT, POST, PATCH, DELETE'
+//     )
+//     res.setHeader(
+//         'Access-Control-Allow-Headers',
+//         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//     )
+//     next()
+// })
 
 app.use('/api/user/watchlist', watchlistRoutes)
 app.use('/api/stock', stockRoutes)
