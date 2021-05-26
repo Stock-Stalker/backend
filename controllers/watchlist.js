695039ce0566ae491065b6c670fa1c2d3eb4ba4e
Starlight Romero <starlightromero@protonmail.com>
2021-05-25 22:15:44 -0700

    [update] watchlist validations - throw error if not validated

--- controllers/watchlist.js
+++ controllers/watchlist.js
@@ -2,6 +2,8 @@ const { updateWatchlistDetails } = require('../utils/watchlist')
 const User = require('../models/user')
 const Symbol = require('../models/symbol')
 
+const { validationResult } = require('express-validator')
+
 exports.getWatchlist = async (req, res) => {
     try {
         let user = await User.findOne({ _id: req.userId })
@@ -13,11 +15,17 @@ exports.getWatchlist = async (req, res) => {
 }
 
 exports.updateWatchlist = async (req, res) => {
-    let symbol = req.body.symbol
-    typeof symbol === 'string' ? (symbol = symbol.toUpperCase()) : (symbol = '')
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation failed.')
+        error.statusCode = 422
+        error.data = errors.array()
+        throw error
+    }
+    const { symbol } = req.body
     try {
         const stock = await Symbol.findOne({
-            symbol: symbol
+            symbol: symbol.toUpperCase()
         })
         let user = await User.findOne({ _id: req.userId, watchlist: stock })
         if (user) {
