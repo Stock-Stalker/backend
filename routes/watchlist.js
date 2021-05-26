57a9bf1a3aa17f8be1f1f5ee88ec40d3b89ecaba
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-05-25 19:51:43 -0700

    [update] PATCH /user/watchlist to ensure sanitization of inputs.

--- routes/watchlist.js
+++ routes/watchlist.js
@@ -2,12 +2,26 @@ const express = require('express')
 
 const router = express.Router()
 
+const { body } = require('express-validator')
+
 const watchlistController = require('../controllers/watchlist')
 
 const isAuth = require('../middleware/isAuth')
 
 router.get('/', isAuth, watchlistController.getWatchlist)
 
-router.patch('/', isAuth, watchlistController.updateWatchlist)
+router.patch('/', [
+  body(
+    'symbol',
+    'Please ensure you pass a valid string as symbol.'
+  )
+    .isLength({ min: 1, max: 6 })
+    .escape()
+    .not()
+    .isEmpty()
+],
+isAuth,
+watchlistController.updateWatchlist
+)
 
 module.exports = router
